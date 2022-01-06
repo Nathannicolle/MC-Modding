25e5cc2449ebab4a43485fcaa85ce130eb0cd5ce
Nathannicolle <nathan-nicolle@orange.fr>
2022-01-06 22:42:15 +0100

    ajout de mon premier mod

---
+++ src/main/java/com/nathannicolle/firstmod/Firstmod.java
@@ -0,0 +1,33 @@
+package com.nathannicolle.firstmod;
+
+import com.nathannicolle.firstmod.init.BlockInit;
+import com.nathannicolle.firstmod.init.ItemInit;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("firstmod")
+public class Firstmod {
+	public static final String MOD_ID = "firstmod";
+	
+	public static final CreativeModeTab FIRSTMOD_TAB = new CreativeModeTab(MOD_ID) {
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public ItemStack makeIcon() {
+			return new ItemStack(ItemInit.EXAMPLE_ITEM.get());
+		}
+	};
+
+	public Firstmod() {
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+		ItemInit.ITEMS.register(bus);
+		BlockInit.BLOCKS.register(bus);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+}
---
+++ src/main/java/com/nathannicolle/firstmod/init/ItemInit.java
@@ -0,0 +1,19 @@
+package com.nathannicolle.firstmod.init;
+
+import com.google.common.base.Supplier;
+import com.nathannicolle.firstmod.Firstmod;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ItemInit {
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Firstmod.MOD_ID);
+	
+	public static final RegistryObject<Item> EXAMPLE_ITEM = register("example_item", () -> new Item(new Item.Properties().tab(Firstmod.FIRSTMOD_TAB)));
+	
+	private static <T extends Item> RegistryObject<T> register(final String name, final Supplier<T> item) {
+		return ITEMS.register(name, item);
+	}
+}
